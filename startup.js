64108dad69062bd8210e150cdd7f491c5944c223
davelockhartmohawkind <dave_lockhart@mohawkind.com>
2014-09-29 16:27:05 -0400

    v17

--- startup.js
+++ startup.js
@@ -140,8 +140,9 @@ var getConnectionSpeed = function () {
                     }
                 }
 
+                //var ref = window.open('http://mobilepricingdev.mohawkind.com/Home/Login', '_self', 'toolbar=no,location=no');
                 var ref = window.open('http://mobilepricingdev.mohawkind.com/Home/Login', '_self', 'toolbar=no,location=no');
-                var ref = cordova.exec(cb,cb, 'InAppBrowser', 'open', ['http://mobilepricingdev.mohawkind.com/Home/Login', '_self', 'toolbar=no,location=no']);
+              //  var ref = cordova.exec(cb,cb, 'InAppBrowser', 'open', ['http://mobilepricingdev.mohawkind.com/Home/Login', '_self', 'toolbar=no,location=no']);
          //   }
           //  catch (err) {
          //       alert(err.message);
