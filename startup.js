e394f6d49a3fec874e3bd4227b3a02a64f926f61
davelockhartmohawkind <dave_lockhart@mohawkind.com>
2014-09-30 16:03:33 -0400

    networkstatus
    networkstatus

--- startup.js
+++ startup.js
@@ -33,19 +33,24 @@ $(document).ready(function () {
 // Functions
 
 var checkConnection = function () {
-    alert('checking connection');
-    var networkState = navigator.network.connection.type;
+    try{
+        alert('checking connection');
+        var networkState = navigator.NetworkStatus.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN] = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI] = 'WiFi connection';
-    states[Connection.CELL_2G] = 'Cell 2G connection';
-    states[Connection.CELL_3G] = 'Cell 3G connection';
-    states[Connection.CELL_4G] = 'Cell 4G connection';
-    states[Connection.NONE] = 'No network connection';
+        var states = {};
+        states[Connection.UNKNOWN] = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.NONE] = 'No network connection';
 
-    alert('Connection type: ' + states[networkState]);
+        alert('Connection type: ' + states[networkState]);
+    }
+    catch (err) {
+        alert(err.message);
+    }
 }
 
 
